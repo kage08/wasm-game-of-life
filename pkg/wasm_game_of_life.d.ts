6304c562d52ab061f6ef96121e8995b788c88428
kage08 <harshavardhan864.hk@gmail.com>
2021-10-24 23:43:31 -0400

    Initial Pkg

---
+++ pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,49 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export enum Cell {
+  Dead,
+  Alive,
+}
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {string}
+*/
+  render(): string;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* @returns {number}
+*/
+  cells(): number;
+/**
+* @param {number} width
+*/
+  set_width(width: number): void;
+/**
+* @param {number} height
+*/
+  set_height(height: number): void;
+/**
+* @param {number} row
+* @param {number} col
+*/
+  toggle_cell(row: number, col: number): void;
+}
